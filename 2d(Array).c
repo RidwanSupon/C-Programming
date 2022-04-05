005fc197d27aa895b6b716306dd76bb3f54719c3
Md.Ridwanur R. Mazumder <98765790+RidwanSupon@users.noreply.github.com>
2022-04-05 16:06:58 +0600

    Add files via upload

---
+++ 2d(Array).c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main(){
+
+
+    int arr[2][3];
+
+    int i,j;
+
+    for(int i=0;i<2;i++){
+        for(int j=0;j<3;j++){
+
+        printf("arr[%d][%d]: ",i,j);
+        scanf("%d",&arr[i][j]);
+        }
+         }
+
+         for(i=0;i<2;i++){
+          for(j=0;j<3;j++){
+              printf("%d",arr[i][j]);
+          if(j==2){
+          printf("\n");
+          }
+
+          }
+
+         }
+
+    }
+
