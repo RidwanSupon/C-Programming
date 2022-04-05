47a71518b7b798c61138c32d6b7f60d96324e6da
Md.Ridwanur R. Mazumder <98765790+RidwanSupon@users.noreply.github.com>
2022-04-05 16:08:25 +0600

    Add files via upload

--- 2d(Array).c
+++ 2d(Array).c
@@ -16,7 +16,7 @@ int main(){
 
          for(i=0;i<2;i++){
           for(j=0;j<3;j++){
-              printf("%d",arr[i][j]);
+              printf("%d\t",arr[i][j]);
           if(j==2){
           printf("\n");
           }
